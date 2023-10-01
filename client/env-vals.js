b8ecd90292b5a1b591c6e76194d64a6bccb5b201
Atsushi Fujioka <atsushi3233@gmail.com>
2023-10-01 17:32:36 +0900

    WebページをHPより取得、AWSリソースをtemplateに作成

---
+++ client/env-vals.js
@@ -0,0 +1,13 @@
+/**
+ * Environment variables
+ * 環境変数
+ */
+// for Amazon Cognito configuration
+export const USERPOOL_DOMAIN = "<ドメイン>";
+export const USERPOOL_REGION = "<リージョン>";
+export const USERPOOL_CLIENT_ID = "<クライアントID>";
+export const USERPOOL_RESPONSE_TYPE = "code";
+export const USERPOOL_SCOPE = "openid";
+
+// for Amazon API Gateway configuration
+export const EXECUTE_API_STAGE = "/<ステージ>";
